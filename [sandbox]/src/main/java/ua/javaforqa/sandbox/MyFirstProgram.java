5fb615e7b541cbdb048215ea16ec598e7751719c
EugeneQA89 <eugen.krasnokutskiy@gmail.com>
2016-09-21 18:44:17 +0300

    Variables practice #1

--- [sandbox]/src/main/java/ua/javaforqa/sandbox/MyFirstProgram.java
+++ [sandbox]/src/main/java/ua/javaforqa/sandbox/MyFirstProgram.java
@@ -4,7 +4,11 @@ public class MyFirstProgram {
 	
 	public static void main (String[] args) {
 
-System.out.println("Hello, world!");
-	}
+        System.out.println("Hello, " + "world!");
+
+        int l = 8;
+        int s = l * l;
+        System.out.println("площать квадрата со стороной " + l + " = " + s);
+    }
 	
 }
