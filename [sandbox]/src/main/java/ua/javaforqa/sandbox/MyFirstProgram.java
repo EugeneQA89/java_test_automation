d08615da2cae69b15d9ada13ca6aa836c5ad5871
EugeneQA89 <eugen.krasnokutskiy@gmail.com>
2016-09-27 11:52:23 +0300

    Several functions added

--- [sandbox]/src/main/java/ua/javaforqa/sandbox/MyFirstProgram.java
+++ [sandbox]/src/main/java/ua/javaforqa/sandbox/MyFirstProgram.java
@@ -3,12 +3,28 @@ package ua.javaforqa.sandbox;
 public class MyFirstProgram {
 	
 	public static void main (String[] args) {
+        hello("world");
+        hello("user");
+        hello("QA");
 
-        System.out.println("Hello, " + "world!");
+        double l = 5;
+        System.out.println("Площать квадрата со стороной " + l + " = " + area(l));
 
-        int l = 8;
-        int s = l * l;
-        System.out.println("площать квадрата со стороной " + l + " = " + s);
+        double a = 4;
+        double b = 6;
+        System.out.println( "Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
     }
-	
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+    public static double area(double a, double b) {
+        return a * b;
+    }
+
 }
